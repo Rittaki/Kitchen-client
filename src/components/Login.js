162253e08e645f4e8c78c83fe97aa709620c6ba7
Rittaki <com.margo@gmail.com>
2022-11-30 22:49:57 +0200

    added files

---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='login-form'>
+      <form>
+      <div class="imgcontainer">
+        <img src='/images/profile.png' alt="Avatar" class="avatar"/>
+      </div>
+        <div className='container'>
+          <label htmlFor='uname'><b>Username</b></label>
+          <input type='text' name='uname' placeholder='Enter username' required/>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required/>
+
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember"/> Remember me
+          </label>
+        </div>
+        <div className="container">
+            <span className="register"><a href="#">Register</a></span>
+            <span className="psw">Forgot <a href="#">password?</a></span>
+          </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login

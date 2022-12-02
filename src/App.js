4131d7f3f93d716d27c6eeca57b503a04a8f072a
Rittaki <com.margo@gmail.com>
2022-12-02 15:48:27 +0200

    fetches api/auth/login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import Login from "./components/Login";
 
 function App() {
+
+  // const fdata = new FormData();
+  // fdata.append('username', 'handuldul@gmail.com');
+  // fdata.append('password', '123456789');
+  const reqBody = {
+    username: "Cook_Programmer@somewhere.com",
+    password: "RecipeInBinary",
+  };
+  console.log(reqBody);
+
+  fetch('/api/auth/login', {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqBody),
+  })
+  .then((response) => Promise.all([response.json(), response.headers]))
+  .then(([body, headers]) => {
+    const authValue = headers.get("authorization");
+    console.log(authValue);
+    // console.log(body);
+  });
+  
+
   return (
     <div>
-      <Login />
+       <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
 import React from 'react'
 import './Login.css'
 
 function Login() {
+  const [userName , setUserName] = useState("");
+  const [password , setPassword] = useState("");
+
+
+  function sendLoginRequest() {
+    console.log("Sending login request");
+  }
+
   return (
     <div className='login-form'>
       <form>
@@ -15,7 +24,7 @@ function Login() {
           <label for="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="psw" required/>
 
-          <button type="submit">Login</button>
+          <button type="submit" onClick={() => sendLoginRequest()}>Login</button>
           <label>
             <input type="checkbox" checked="checked" name="remember"/> Remember me
           </label>
